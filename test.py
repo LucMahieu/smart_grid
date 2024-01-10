5b481fdd6b0013d912a6153942b2ba7f143da646
Luc Mahieu <lucmauricemahieu@gmail.com>
2024-01-10 13:14:45 +0100

    test 2

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-class Battery():
-    def __init__(self) -> None:
-        pass
+class House():
+    def __init__(self, pos_y, pos_x, solar_output) -> None:
+        self.pos_y = pos_y
+        self.pos_x = pos_x
+        self.solar_output = solar_output
+
