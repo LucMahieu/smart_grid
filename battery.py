a04a4f92bd31a27186a287b486018e0a7e36558e
ayseacar1 <155972618+ayseacar1@users.noreply.github.com>
2024-01-12 00:10:20 +0100

    Update battery.py

--- battery.py
+++ battery.py
@@ -4,10 +4,14 @@ class Battery():
         self.position_x = position_x
         self.position_y = position_y
         self.capacity = capacity
+        self.connected_houses = []
 
-    
+    def connect_house(self, house):
+        self.connected_houses.append(house)
+            
     # def capacity_limit(self):
         """
         Calculates the output the battery recieves from the houses,
         and checks if the capacity limit of the battery is exceeded.
         """
+        total_output = sumhouse.max
