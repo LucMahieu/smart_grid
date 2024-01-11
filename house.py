1709ce3e973d463f83e3bebb4960fc66f87b0fb4
ditavanleeuwen <117078034+ditavanleeuwen@users.noreply.github.com>
2024-01-11 15:14:59 +0100

    Update house.py

---
+++ house.py
@@ -0,0 +1,5 @@
+class House:
+    def __init__(self, x_position, y_position, max_output):
+        self.x_position = x_position
+        self.y_position = y_position
+        self.max_output = max_output
