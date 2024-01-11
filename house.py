2c098d5c1adb83a44db6f861aa7ef4067173ba21
ayseacar1 <155972618+ayseacar1@users.noreply.github.com>
2024-01-11 23:58:15 +0100

    Update house.py

--- house.py
+++ house.py
@@ -3,3 +3,11 @@ class House:
         self.x_position = x_position
         self.y_position = y_position
         self.max_output = max_output
+        self.battery = None
+
+    def connect_to_battery(self, battery):
+        self.battery = battery
+
+
+    def calculate_distance(self, other_house):
+        return abs(self.x_position - other_house.x_position) + abs(self.y_position - other_house.y_position)
