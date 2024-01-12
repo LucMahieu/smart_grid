c210a307e8b8d945083335393441e824607f6172
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-12 15:06:37 +0100

    removing houses.svg & adding __init__.py

---
+++ export_json.py
@@ -0,0 +1,36 @@
+from classes.district import District
+from collections import OrderedDict
+import json
+
+
+
+
+def battery_to_dict(battery):
+    """Converts the attributes of a class to a dictionary."""
+    output = OrderedDict()
+    output["location"] = str(battery.pos_x) + "," + str(battery.pos_y)
+    output["capacity"] = battery.capacity
+    output["houses"] = [house_to_dict(house) for house in battery.connected_houses]
+    return output
+
+def district_to_dict(district):
+    """Converts the attributes of a class to a dictionary."""
+    output = OrderedDict()
+    output["district"] = district.name
+    output["costs-shared"] = district.costs_shared
+    output["batteries"] = [battery_to_dict(battery) for battery in district.batteries]
+    return output
+
+def export_json(districts):
+    """Exports the districts to a json file."""
+    output = [district_to_dict(district) for district in districts]
+    with open("output.json", "w") as outfile:
+        json.dump(output, outfile, indent=4, sort_keys=True)
+
+
+if __name__ == "__main__":
+    districts = [District(1, "data/district_1/district-1_batteries.csv", "data/district_1/district-1_houses.csv"),
+                 District(2, "data/district_2/district-2_batteries.csv", "data/district_2/district-2_houses.csv"),
+                 District(3, "data/district_3/district-3_batteries.csv", "data/district_3/district-3_houses.csv")]
+    
+    export_json(districts)
