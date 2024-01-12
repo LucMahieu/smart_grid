ff31f1fc696ff9e16f6cb2275f02bdf30e1bee71
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-12 13:37:01 +0100

    update

--- cable.py
+++ cable.py
@@ -2,8 +2,8 @@ class Cable:
     def __init__(self, pos_x_house, pos_y_house, pos_x_batt, pos_y_batt):
         self.pos_x_house = pos_x_house
         self.pos_y_house = pos_y_house
-        self.pos_x_batt = pos_y_batt
-        self.pos_x_batt = pos_y_batt
+        self.pos_x_batt = pos_x_batt
+        self.pos_y_batt = pos_y_batt
 
     #def __str__(self):
     #    return f"Cable from {self.house_pos_x, self.house_pos_y} to {self.battery_pos_x, self.battery_pos_y}"
