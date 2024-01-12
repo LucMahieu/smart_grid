38e82109cc0f49f4675794fc04e600c953470d09
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-12 13:07:44 +0100

    changing from position to x and y coordinates

--- cable.py
+++ cable.py
@@ -1,8 +1,10 @@
 class Cable:
-    def __init__(self, house_position, battery_position):
-        self.house_position = house_position
-        self.battery_position = battery_position
+    def __init__(self, house_position_x, self.house_position_y, battery_position_x, battery_position_y):
+        self.house_position_x = house_position_x
+        self.house_position_y = house_position_y
+        self.battery_position_x = battery_position_y
+        self.battery_position_x = battery_position_y
 
     #def __str__(self):
-    #    return f"Cable from {self.house_position} to {self.battery_position}"
+    #    return f"Cable from {self.house_position_x, self.house_position_y} to {self.battery_position_x, self.battery_position_y}"
 
