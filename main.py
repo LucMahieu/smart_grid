aecf52f7997fb10368067c279f645f04bef66552
ayseacar1 <155972618+ayseacar1@users.noreply.github.com>
2024-01-12 12:49:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-class House():
-    def __init__(self, pos_y, pos_x, solar_output) -> None:
-        self.pos_y = pos_y
-        self.pos_x = pos_x
-        self.solar_output = solar_output
 
-
-if __name__ == "__main__":
