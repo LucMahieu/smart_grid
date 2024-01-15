f99ccf35e48e75d6e12a6fc44b5ce2ce0f70ed13
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-15 14:26:23 +0100

    adding random connections

--- algorithms/randomise_connections.py
+++ algorithms/randomise_connections.py
@@ -1,1 +1,30 @@
 import random
+from classes.district import District  
+
+def random_assignment(batteries, houses):
+    '''
+    Randomly assign each house to batteryi now want 
+    '''
+    for house in houses:
+        # randomly select battery
+        selected_battery = random.choice(batteries)
+        
+        # if battery does not have capacity left based on output selected house, keep selecting a random house
+        while capacity_check(selected_battery, house) == False:
+            selected_battery = random.choice(batteries)
+
+        house.battery = selected_battery
+
+
+def capacity_check(battery, house):
+    '''
+    Checks if assigned battery has capacity left to be connected to new house
+    '''
+    if battery.capacity >= house.max_output:
+        # updating battery capacity
+        battery.capacity -= house.max_output
+        return True 
+    
+    else:
+        return False
+
