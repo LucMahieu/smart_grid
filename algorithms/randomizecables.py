f689563b636be473047df71b5d48d5e68a0f13cd
ayseacar1 <ayse.acar.acar@student.uva.nl>
2024-01-15 12:49:12 +0100

    update

--- algorithms/randomizecables.py
+++ algorithms/randomizecables.py
@@ -1,1 +1,21 @@
+from classes.cable import Cable
+
+
 # function that randomizes cables
+def random_cables(district, connections):
+    cables = []
+    for connection in connections:
+        house = connection['house']
+        battery = connection ['battery']
+
+        # generate and lay the cable
+        cable_route = {
+            'start_x': house.pos_x,
+            'start_y': house.pos_y,
+            'end_x': battery.pos_x,
+            'end_y': battery.pos_y
+        }
+
+        #lay cables and save info
+        cable = Cable(
+
