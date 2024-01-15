73a0379740a87ef97c546f2322ad1fa3df704582
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-15 14:42:33 +0100

    update

--- algorithms/randomise_cables.py
+++ algorithms/randomise_cables.py
@@ -34,31 +34,3 @@ def random_cables(houses):
             (cable_route['end_x'], cable_route['end_y']))
 
         
-
-    '''
-    Randomly assign each house to batteryi now want 
-    '''
-    for house in houses:
-        # randomly select battery
-        selected_battery = random.choice(batteries)
-        
-        # if battery does not have capacity left based on output selected house, keep selecting a random house
-        while capacity_check(selected_battery, house) == False:
-            selected_battery = random.choice(batteries)
-
-        house.battery = selected_battery
-
-
-def capacity_check(battery, house):
-    '''
-    Checks if assigned battery has capacity left to be connected to new house
-    '''
-    if battery.capacity >= house.max_output:
-        # updating battery capacity
-        battery.capacity -= house.max_output
-        return True 
-    
-    else:
-        return False
-
-
