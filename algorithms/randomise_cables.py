36f1131acc9fd1f8fa5f7a63cbbdf1402671985a
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-15 16:47:47 +0100

    adding randomly laying cables

--- algorithms/randomise_cables.py
+++ algorithms/randomise_cables.py
@@ -1,10 +1,22 @@
 import random
 from classes.district import District 
 
-# function that randomizes cables
-def random_cables(houses):
+def random_segment(current, end):
     '''
-    Randomly lay cable from house to selected battery
+    Randomly choosing where to lay a single cablesegment within 50 x 50 grid until battery is reached
+    '''
+    if current == end:
+        return 0
+    elif current == 0:
+        return random.choice([0, 1])
+    elif current == 50:
+        return random.choice([0, -1])
+    else:
+        return random.choice([-1, 0, 1])
+
+def laying_cables(houses):
+    '''
+    Lay cable from house to selected battery
     '''
     for house in houses:
 
@@ -15,24 +27,45 @@ def random_cables(houses):
         cable_end_y = house.battery.pos_y
 
         # generate random route 
-        cable_route = [cable_start_x, cable_start_y]
+        cable_route = [(cable_start_x, cable_start_y)]
         cable_x = cable_start_x
         cable_y = cable_start_y
 
-        # kaying cable untill battery is reached 
+        # laying cable untill battery is reached 
         while (cable_x, cable_y) != (cable_end_x, cable_end_y):
-            if ...:
-                cable_x += 1
-            else:
-                cable_x -= 1
-        
-            if ...: 
-                cable_y += 1
-            else:
-                cable_y -= 1
-
-            cable_route.append(cable_x, cable_y)
+            cable_x += random_segment(cable_x, cable_end_x)
+            cable_y += random_segment(cable_y, cable_end_y)
+            cable_route.append((cable_x, cable_y))
             
+            # if cable_x == 50:
+            #     cable_x += random.choice([0, -1])
+            # elif cable_x == 0:
+            #     cable_x += random.choice([0, 1])
+            # else:
+            #     cable_x += random.choice([-1, 0, 1])
+
+            # if cable_y == 50:
+            #     cable_y += random.choice([0, -1])
+            # elif cable_y == 0:
+            #     cable_y += random.choice([0, 1])
+            # else:
+            #     cable_y += random.choice([-1, 0, 1])
+
+            # cable_route.append((cable_x, cable_y))
+
+            # cable_x += 1 if random.choice([True, False]) and cable_x < 50 else -1 if cable_x > 0 else 0
+            # cable_y += 1 if random.choice([True, False]) and cable_y < 50 else -1 if cable_y > 0 else 0
+
+            # if ...:
+            #     cable_x += 1
+            # else:
+            #     cable_x -= 1
+        
+            # if ...: 
+            #     cable_y += 1
+            # else:
+            #     cable_y -= 1
+
         # #lay cables and save info
         # cable = Cable((cable_route['start_x'], cable_route['start_y']),
         #     (cable_route['end_x'], cable_route['end_y']))
