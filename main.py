f7eb9cae33412a8a123067142a36e9cb5679bc7c
ayseacar1 <ayse.acar.acar@student.uva.nl>
2024-01-16 13:58:40 +0100

    update

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from algorithms.randomize_connections import random_assignment
 if __name__ == "__main__":
     # create districts from files with batteries and houses
     districts = [
-        District("data/district_1/district-1_batteries.csv", "data/district_1/district-1_houses.csv"),
-        District("data/district_2/district-2_batteries.csv", "data/district_2/district-2_houses.csv"),
-        District("data/district_3/district-3_batteries.csv", "data/district_3/district-3_houses.csv")
-    ]
+        District(1, "data/district_1/district-1_batteries.csv", "data/district_1/district-1_houses.csv"),
+        District(2, "data/district_2/district-2_batteries.csv", "data/district_2/district-2_houses.csv"),
+        District(3, "data/district_3/district-3_batteries.csv", "data/district_3/district-3_houses.csv")
+    
     # connect houses with batteries in a district
     for district in districts:
         random_assignment(district)
