6ca62d67c7b4631bc00edfcf5479829a446696b7
Luc Mahieu <lucmauricemahieu@gmail.com>
2024-01-17 16:44:11 +0100

    random cable laying functions working beautifully

--- algorithms/randomize.py
+++ algorithms/randomize.py
@@ -1,9 +1,12 @@
 import random
-from algorithms.random_steps import determine_next_step
+# from algorithms.random_steps import determine_next_step
 
 class Random_algo():
-    def __init__(self):
-        pass
+    def __init__(self, grid_size=50):
+        self.outer_grid = self.create_outer_grid(grid_size)
+        # use middle of grid as starting point, based on grid size
+        self.current_pos = (grid_size // 2, grid_size // 2)
+        self.options = set()
 
 
     def run(self, district):
@@ -13,11 +16,109 @@ class Random_algo():
         for house in district.houses:
             district.battery_houses_connections
             house.battery = self.random_assignment(district, house)
-            # house.cables = self.random_cables(house)
-            house.cables = self.random_cables_v2(house)
+            house.cables = self.random_cables(house)
     
-        return district 
+        return district
     
+
+    def create_outer_grid(self, grid_size):
+        """
+        Creates a set of points that are just outside the grid.
+        """	
+        outer_grid = set()
+
+        for x in range(1, grid_size + 1):
+            outer_grid.add((x, 0))  # Points just above the top row
+            outer_grid.add((x, grid_size + 1))  # Points just below the bottom row
+        
+        for y in range(1, grid_size + 1):
+            outer_grid.add((0, y))  # Points just left of the leftmost column
+            outer_grid.add((grid_size + 1, y))  # Points just right of the rightmost column
+        
+        return outer_grid
+    
+
+    # def determine_possible_positions(self, prev_x, prev_y, current_x, current_y):
+    #     '''
+    #     Determines possible positions for the next step.
+    #     '''
+    #     # possible movements
+    #     right = (1, 0)
+    #     left = (-1, 0)
+    #     up = (0, 1)
+    #     down = (0, -1)
+
+    #     # determine current pos (TEMPORARY LINE > REMOVE LATER when current_pos is an attribute of randomize.py)
+    #     self.current_pos = (current_x, current_y)
+
+    #     # define new position options based on current position
+    #     self.options = set()
+    #     for position in [right, left, up, down]:
+    #         self.options.add((self.current_pos[0] + position[0], self.current_pos[1] + position[1]))
+
+    #     # determine previous pos (TEMPORARY LINE > REMOVE LATER when prev_pos is an attribute of randomize.py)
+    #     self.prev_pos = (prev_x, prev_y)
+
+    #     # narrow down options to those that are within the grid and which are not its previous positions
+    #     print(f"Options: {self.options}")
+
+    #     # remove options that are in outer grid
+    #     for option in self.options.copy():
+    #         if option in self.outer_grid:
+    #             self.options.remove(option)
+    #         if option == self.prev_pos:
+    #             self.options.remove(option)
+
+    #     print(f"Options: {self.options}")
+
+    #     # randomly choose the next position from options
+    #     self.new_pos = random.choice(list(self.options))
+
+    #     print(f"New position: {self.new_pos}")
+
+    #     # NOT YET NECESSARY, BUT WILL BE LATER when the attributes of randomize.py are embedded in random_cables_v2
+    #     self.current_pos = self.new_pos
+
+    #     # return new position (TEMPORARY LINE > REMOVE LATER when new_pos is attribute of randomize.py and is embedded in random_cables_v2)
+    #     return self.new_pos[0], self.new_pos[1]
+    
+
+    def determine_possible_positions(self, prev_x, prev_y, current_x, current_y):
+        '''
+        Determines possible positions for the next step.
+        '''
+        # possible movements
+        right = (1, 0)
+        left = (-1, 0)
+        up = (0, 1)
+        down = (0, -1)
+
+        # determine current pos (TEMPORARY LINE > REMOVE LATER when current_pos is an attribute of randomize.py)
+        self.current_pos = (current_x, current_y)
+
+        # define new position options based on current position
+        self.options = set()
+        for position in [right, left, up, down]:
+            self.options.add((self.current_pos[0] + position[0], self.current_pos[1] + position[1]))
+
+        # determine previous pos (TEMPORARY LINE > REMOVE LATER when prev_pos is an attribute of randomize.py)
+        self.prev_pos = (prev_x, prev_y)
+
+        # remove options that are in outer grid
+        for option in self.options.copy():
+            if option in self.outer_grid:
+                self.options.remove(option)
+            if option == self.prev_pos:
+                self.options.remove(option)
+
+        # randomly choose the next position from options
+        self.new_pos = random.choice(list(self.options))
+
+        # NOT YET NECESSARY, BUT WILL BE LATER when the attributes of randomize.py are embedded in random_cables_v2
+        self.current_pos = self.new_pos
+
+        # return new position (TEMPORARY LINE > REMOVE LATER when new_pos is attribute of randomize.py and is embedded in random_cables_v2)
+        return self.new_pos[0], self.new_pos[1]
 
     def random_assignment(self, district, house):
         '''
@@ -31,8 +132,8 @@ class Random_algo():
 
         # keep selecting random battery until a battery with enough capacity is found
         while enough_capacity == False:
-            print(len(batteries_copy))
 
+            # stop if no battery has enough capacity
             if len(batteries_copy) == 0:
                 print("No battery has enough capacity")
                 break
@@ -52,59 +153,7 @@ class Random_algo():
         return selected_battery
 
 
-    def random_segment(self, current, end):
-        '''
-        Chooses randomly where to lay a single new cablesegment within 50 x 50 grid.
-        '''
-        # Stay in same position if battery is reached
-        if current == end:
-            return 0
-        
-        # Go forward or stay in same position at start of grid
-        elif current == 0:
-            return random.choice([0, 1])
-        
-        # Go backward or stay in same position at end of grid
-        elif current == 50: 
-            return random.choice([0, -1])
-        
-        # Move in any direction
-        else:
-            return random.choice([-1, 0, 1])
-
-
     def random_cables(self, house):
-        '''
-        Lay cable from house to selected battery.
-        '''
-        # Cable starts at house and ends at battery
-        cable_start_x = house.pos_x
-        cable_start_y = house.pos_y
-        cable_end_x = house.battery.pos_x
-        cable_end_y = house.battery.pos_y
-        cable_route = [(cable_start_x, cable_start_y)]
-
-        # starting position of cable
-        cable_x = cable_start_x
-        cable_y = cable_start_y
-
-        # Keep generating and adding cable segments untill battery is reached
-        while (cable_x, cable_y) != (cable_end_x, cable_end_y):
-            # Generate random segment (step)
-            cable_x += self.random_segment(cable_x, cable_end_x)
-            cable_y += self.random_segment(cable_y, cable_end_y)
-
-            # Add segment to cable route
-            cable_route.append((cable_x, cable_y))
-            
-        return cable_route 
-    
-
-
-# ---------------------------------------------------------------------------------------------
-    # new version of random_cables
-
-    def random_cables_v2(self, house):
         '''
         Lay cable from house to selected battery.
         '''
@@ -127,8 +176,8 @@ class Random_algo():
 
         # keep generating and adding cable segments untill battery is reached
         while (current_x, current_y) != (cable_end_x, cable_end_y):
-            # Generate random segment (step)
-            new_x, new_y = determine_next_step(prev_x, prev_y, current_x, current_y)
+            # Determine possible positions and choose one randomly
+            new_x, new_y = self.determine_possible_positions(prev_x, prev_y, current_x, current_y)
 
             # Add segment to cable route
             cable_route.append((new_x, new_y))
@@ -142,3 +191,54 @@ class Random_algo():
             current_y = new_y
             
         return cable_route
+
+
+#---------------------------------------------------------------------------------------------
+
+# ARCHIVED CODE
+    
+    # def random_cables(self, house):
+    #     '''
+    #     Lay cable from house to selected battery.
+    #     '''
+    #     # Cable starts at house and ends at battery
+    #     cable_start_x = house.pos_x
+    #     cable_start_y = house.pos_y
+    #     cable_end_x = house.battery.pos_x
+    #     cable_end_y = house.battery.pos_y
+    #     cable_route = [(cable_start_x, cable_start_y)]
+
+    #     # starting position of cable
+    #     cable_x = cable_start_x
+    #     cable_y = cable_start_y
+
+    #     # Keep generating and adding cable segments untill battery is reached
+    #     while (cable_x, cable_y) != (cable_end_x, cable_end_y):
+    #         # Generate random segment (step)
+    #         cable_x += self.random_segment(cable_x, cable_end_x)
+    #         cable_y += self.random_segment(cable_y, cable_end_y)
+
+    #         # Add segment to cable route
+    #         cable_route.append((cable_x, cable_y))
+            
+    #     return cable_route
+
+    # def random_segment(self, current, end):
+    #     '''
+    #     Chooses randomly where to lay a single new cablesegment within 50 x 50 grid.
+    #     '''
+    #     # Stay in same position if battery is reached
+    #     if current == end:
+    #         return 0
+        
+    #     # Go forward or stay in same position at start of grid
+    #     elif current == 0:
+    #         return random.choice([0, 1])
+        
+    #     # Go backward or stay in same position at end of grid
+    #     elif current == 50: 
+    #         return random.choice([0, -1])
+        
+    #     # Move in any direction
+    #     else:
+    #         return random.choice([-1, 0, 1])
