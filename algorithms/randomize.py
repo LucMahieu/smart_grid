e4cff3c622691008205e357f6eacd463c74b1fc3
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-17 08:55:59 +0100

    small style fix

--- algorithms/randomize.py
+++ algorithms/randomize.py
@@ -21,13 +21,13 @@ class Random_algo():
         '''
         Randomly assigns each house to a battery that meets the requirements (e.g. enough capacity).
         '''
-        # make copy of batteries list to keep track of batteries that have been tried
+        # Make copy of batteries list to keep track of batteries that have been tried
         batteries_copy = district.batteries.copy()
 
-        # set enough_capacity to False to start while loop
+        # Set enough_capacity to False to start while loop
         enough_capacity = False
 
-        # keep selecting random battery until a battery with enough capacity is found
+        # Keep selecting random battery until a battery with enough capacity is found
         while enough_capacity == False:
             print(len(batteries_copy))
 
@@ -35,16 +35,16 @@ class Random_algo():
                 print("No battery has enough capacity")
                 break
 
-            # select random battery from shrinking list of batteries
+            # Select random battery from shrinking list of batteries
             selected_battery = random.choice(batteries_copy)
 
-            # check if battery has enough capacity
+            # Check if battery has enough capacity
             enough_capacity = selected_battery.capacity_check(house)
 
-            # remove battery from list of batteries because it doesn't have enough capacity
+            # Remove battery from list of batteries because it doesn't have enough capacity
             batteries_copy.remove(selected_battery)
 
-        # add current house to the list of houses that are connected to the selected battery
+        # Add current house to the list of houses that are connected to the selected battery
         district.battery_houses_connections[selected_battery].append(house)
 
         return selected_battery
