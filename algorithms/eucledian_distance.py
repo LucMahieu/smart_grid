9f1f0c3e6449df336ef021283ae7b9ce3d8af9a3
ditavanleeuwen <ditavleeuwen@gmail.com>
2024-01-18 11:55:05 +0100

    eucledian update

--- algorithms/eucledian_distance.py
+++ algorithms/eucledian_distance.py
@@ -1,4 +1,5 @@
 import math
+import random
 from algorithms.randomize.py import Random_algo() 
 
 def eucledian_distance(house, battery):
@@ -14,6 +15,7 @@ def eucledian_distance(house, battery):
 
 def min_distance(batteries, houses):
     '''
+    Saves dictionary with closest battery option for each house
     '''
     best_option_dict = {}
     for house in houses:
@@ -28,25 +30,107 @@ def min_distance(batteries, houses):
 
     return best_option_dict
 
-def greedy_distance(batteries, houses):
+def closest_connection(batteries, houses):
     '''
+    Connects a house to the closest battery with enough capacity left
     '''
-    min_distance_dict = min_distance(batteries, houses)
+    #min_distance_dict = min_distance(batteries, houses)
+    houses_copy = copy.deepcopy(houses)
+    
+    # Shuffle the copied list
+    random.shuffle(houses_copy)
 
-    for house in houses:
+    for house in houses_copy:
+
+        # Create a new list to store batteries with enough capacity
+        batteries_with_capacity = []
+
+        for battery in batteries:
+            
+            # Check if battery has enough capacity
+            enough_capacity = battery.check_capacity(house)
+
+            if enough_capacity:
+                batteries_with_capacity.append(battery)
+
+        # Use batteries with enough capacity to calculate distance
+        # for battery in batteries_with_capacity
+        if batteries_with_capacity:
         
-        capacity_check_passed = False
-
-        while capacity_check_passed == False:
+            # Connecting to closest battery
+            min_distance_dict = min_distance(batteries_with_capacity, houses)
             house.battery = min_distance_dict[house]
-        
-            if capacity_check(house) == True:
-                house.cables = random_cables(house)
-                capacity_check_passed = True  
 
-            else:
-                # Remove selected battery from dictionary
-                min_distance_dict[house].pop()
+            # Updating capacity
+            house.battery.update_capacity(house)
+
+        # # Updating capacity if there is enough for new connection
+        # if house.battery.check_capacity(house) == True:
+        #     house.battery.update_capacity(house)
+
+        # # Removing battery as option if not enough capacity and repeating process
+        # else:
+        #     batteries_copy = copy.deepcopy(batteries)
+        #     batteries_copy.pop(batteries_copy.index(house.battery))
+        #     min_distance_dict = min_distance(batteries_copy, houses)
+        #     house.battery = min_distance_dict[house]
+        
+        # capacity_check_passed = False
+
+        # while capacity_check_passed == False:
+        #     house.battery = min_distance_dict[house]
+        
+        #     if capacity_check(house) == True:
+        #         house.cables = random_cables(house)
+        #         capacity_check_passed = True  
+        #         house.battery.capacity = update_capacity(house):
+
+        #     else:
+        #         # Remove selected battery from dictionary
+        #         min_distance_dict[house].pop()
 
     
         
+# def min_distance(batteries, houses):
+#     '''
+#     Saves dictionary with closest battery option for each house
+#     '''
+#     best_option_dict = {}
+#     for house in houses:
+#         distance_dict = {}
+#         for battery in batteries:
+#             distance = eucledian_distance(house, battery)
+#             distance_dict[battery] = distance
+
+#         # Saving order of distances
+#         sorted_batteries = sorted(distance_dict, key=distance_dict.get)
+#         best_option_dict[house] = sorted_batteries 
+
+#     return best_option_dict
+            
+# def closest_connection(batteries, houses):
+#     '''
+#     Connects a house to the closest battery with enough capacity left
+#     '''
+#     min_distance_dict = min_distance(batteries, houses)
+#     houses_copy = copy.deepcopy(houses)
+    
+#     # Shuffle the copied list
+#     random.shuffle(houses_copy)
+
+#     for house in houses_copy:
+#         for battery in min_distance_dict[house]:
+
+#             # Updating capacity if there is enough for new connection
+#             if house.battery.check_capacity(house) == True:
+#                 house.battery.update_capacity(house)
+                
+#                 # Connecting to closest battery
+#                 house.battery = min_distance_dict[house]
+
+#             # Removing battery as option if not enough capacity and repeating process
+#             else:
+#                 batteries_copy = copy.deepcopy(batteries)
+#                 batteries_copy.pop(batteries_copy.index(house.battery))
+#                 min_distance_dict = min_distance(batteries_copy, houses)
+#                 house.battery = min_distance_dict[house]
